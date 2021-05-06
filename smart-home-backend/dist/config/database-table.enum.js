388423577a67556006b35455eeacac0c5ba63ad4
Oliver Dragschitz <66690543+oliverdragschitz@users.noreply.github.com>
2021-05-06 20:47:31 +0200

    Delete smart-home-backend/dist directory

--- smart-home-backend/dist/config/database-table.enum.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.DatabaseTable = void 0;
-var DatabaseTable;
-(function (DatabaseTable) {
-    DatabaseTable["User"] = "User";
-    DatabaseTable["Log"] = "Log";
-    DatabaseTable["Devices"] = "IoTDevices";
-})(DatabaseTable = exports.DatabaseTable || (exports.DatabaseTable = {}));
